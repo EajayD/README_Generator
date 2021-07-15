35588f380114bfc3ad225a64eba11bd8688b3353
EajayD <edelos1524@hotmail.com>
2021-07-14 17:49:17 -0700

    test 2 questions, to see if array of questions is called in terminal

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-// TODO: Include packages needed for this application
+//Package variables
+const inquire = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title',
+    },
+
+    {
+        type: 'input',
+        message: 'Please provide a brief description of your project.',
+        name: 'description',
+    },
+];
+
+inquire.prompt(questions);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
