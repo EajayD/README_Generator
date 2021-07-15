b01c477f9a4cdc1868305ad9789d986f499629de
EajayD <edelos1524@hotmail.com>
2021-07-14 18:42:33 -0700

    added more questions, may need to restructure

--- index.js
+++ index.js
@@ -15,6 +15,49 @@ const questions = [
         message: 'Please provide a brief description of your project.',
         name: 'description',
     },
+
+    {
+        type: 'input',
+        message: 'Please describe the installation process if any.',
+        name: 'install',
+    },    
+
+    {
+        type: 'input',
+        message: 'Please provide instructions on how to use this project.',
+        name: 'usage',
+    },
+
+    {
+        type: 'input',
+        message: 'Please credit yourself and any collaboraters/resources in this section.',
+        name : 'credits',
+    },
+
+    {
+       type: 'list',
+       message: 'Please select a license',
+       choices: ['MIT', 'GNU GPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'Unlicense', 'None'],
+       name: 'license'
+    },
+
+    {
+        type: 'input',
+        message: 'Please provide a test example to showcase the project.',
+        name: 'test',
+    },
+
+    {
+        type: 'input',
+        message: 'Please provide your Github Username',
+        name: 'username',
+    },
+
+    {
+        type: 'input',
+        message: 'Please provide your contact email',
+        name: 'email',
+    },
 ];
 
 inquire.prompt(questions);
